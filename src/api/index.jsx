14f99055551f40db4da8888f81522cdd9d1596c9
Nemesis1019 <caac1019@gmail.com>
2024-02-10 15:49:43 -0500

    Ready for production

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const clientApi=axios.create({
-    baseURL:'http://localhost:8000/client'
+    baseURL:'https://crudback-qx95.onrender.com/client/'
 })
 export const getAllClients =()=>{
     return clientApi.get('/')
@@ -14,7 +14,7 @@ export const deleteClient=(id)=>{
     return clientApi.delete(`/${id}/`)
 }
 export const postClients=(client)=>{
-    return axios.post('http://localhost:8000/client/',client)
+    return axios.post('https://crudback-qx95.onrender.com/client/',client)
 }
 export const updateclient=(id,client)=>{
     return clientApi.put(`/${id}/`, client)
